52b537f43b6fc70c594e1cffc4a039f5c10fc025
Amirouche <amirouche.ibrir@gmail.com>
2022-12-21 12:35:57 +0100

    authentification login.php

--- login.php
+++ login.php
@@ -1,17 +1,29 @@
-<?php $auth = 0; ?>
-<?php include 'lib/includes.php'; ?>
-<?php include 'partials/header.php'; ?>
-
 <?php
-var_dump($_POST);
-$_POST
+$auth = 0; 
+include 'lib/includes.php'; 
+
+
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $db->quote($_POST['username']);
+    $password = sha1($_POST['password']);
+    $sql = "SELECT * FROM users WHERE username= $username AND password = '$password'";
+    $select = $db->query($sql);
+    if ($select->rowCount() > 0) {
+        $_SESSION['Auth'] = $select->fetch();
+        header('Location:' . WEBROOT . 'admin/index.php');
+    }
+}
+
 ?>
+
+include 'partials/header.php'; 
 
 <form action="#" method="POST">
 
     <div class="form-group">
         <label for="username">Nom d'utilisateur</label>
-        <input type="text" class="form-control" id="username" name="username">
+        <?php echo input('username'); ?>
     </div>
 
     <div class="form-group">
@@ -22,4 +34,5 @@ $_POST
     <button type="submit" class="btn btn-default">Se connecter</button>
 </form>
 
+<?php include 'lib/debug.php'; ?>
 <?php include 'partials/footer.php'; ?>
