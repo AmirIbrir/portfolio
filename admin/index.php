6e4509b7923e940474033afdc785e7894ee52ab4
Amirouche <amirouche.ibrir@gmail.com>
2022-12-24 10:58:35 +0100

    ajout fichier constants.php

--- admin/index.php
+++ admin/index.php
@@ -1,16 +1,20 @@
-<?php include '../lib/debug.php'; ?>
+<?php
+include '../lib/includes.php';
+include '../partials/admin_header.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-    <H1>FOOTER</H1>
+    
 </body>
-
 </html>
+
+<?php
+include '../partials/footer.php';
--- lib/constants.php
+++ lib/constants.php
@@ -1,4 +1,14 @@
-<?php 
-define('WEBROOT', dirname($_SERVER['SCRIPT_NAME']). '/');
+
+<?php
+$directory = basename(dirname(dirname(__FILE__))); 
+$url = explode($directory, $_SERVER['REQUEST_URI']);
+//var_dump(basename(dirname(dirname(__FILE__))));
+
+if (count($url) == 1) {
+    define('WEBROOT', '/');
+}else{
+    define('WEBROOT', $url[0] . 'Portfolio/');
+} 
+
 //var_dump(WEBROOT);
 ?>
