26ef09a9905b0023bf1b09ce9e44e6c79e8ed1fb
Amirouche <amirouche.ibrir@gmail.com>
2022-12-26 16:31:28 +0100

    ajout function csrfInput, modif fonction checkCsrf dans auth.php

--- admin/category_edit.php
+++ admin/category_edit.php
@@ -1,5 +1,22 @@
 <?php
 include '../lib/includes.php';
+
+if (isset($_POST['name']) && isset($_POST['slug'])) {
+    checkCsrf();
+    $slug = $_POST['slug'];
+
+    if (preg_match('%[ˆa-zA-Z0-9_-]$%', $slug)) {
+        $name = $db->quote($_POST['name']);
+        $slug = $db->quote($_POST['slug']);
+        $db->query("INSERT INTO categories SET name=$name, slug=$slug");
+        setFlash('La catégorie a bien été ajoutée');
+        header('Location:category.php');
+        die();
+    } else {
+        setFlash('Le slug n\'est pas valide', 'danger');
+    }
+}
+
 include '../partials/admin_header.php';
 ?>
 
@@ -11,12 +28,12 @@ include '../partials/admin_header.php';
         <label for="name">Nom de la catégorie</label>
         <?php echo input('name'); ?>
     </div>
-    
+
     <div class="form-group">
         <label for="slug">URL de la catégorie</label>
         <?php echo input('slug'); ?>
     </div>
-
+    <?= csrfInput(); ?>
     <button type="submit" class="btn btn-default">Enregistrer</button>
 </form>
 
